a0e582d8d9f0cba975919fd9ed62506b93344498
zsebastian1 <44416197+zsebastian1@users.noreply.github.com>
2019-08-07 15:44:56 -0400

    Add files via upload

---
+++ Assignment1_3.java
@@ -0,0 +1,13 @@
+package ACADAssignments;
+
+import java.util.Scanner;
+
+public class Assignment1_3 {
+
+	public static void main(String[] args) {
+
+		System.out.println("A Bitwise 0 & 0 is: " + (0 & 0));
+		System.out.println("A Logical 0 && 0 is: " + (false && false));
+	}
+
+}
